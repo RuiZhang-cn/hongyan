39271d447b19be2d18345099983c9793555ffec1
rui <2450782689@qq.com>
2022-08-17 18:13:17 +0800

    新增注释和默认从Attribute取参

--- src/main/java/com/rui/hongyan/utils/ParameterUtil.java
+++ src/main/java/com/rui/hongyan/utils/ParameterUtil.java
@@ -1,6 +1,8 @@
 package com.rui.hongyan.utils;
 
 import cn.hutool.core.convert.Convert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
@@ -11,11 +13,26 @@ import java.util.Optional;
  * @Description
  * @Date 2022/07/29 下午 11:35
  */
+@Slf4j
 public class ParameterUtil{
-    public static <T> T getOrDefault(Object t, T defaultValue){
+
+    /**
+     * 将Object转换为defaultValue的类型
+     * defaultValue如果是基础类型会进行自动装箱
+     * 如果是数组转字符串会转换成[0, 1]的字符串
+     * 转其他的会报错
+     * @param t 源
+     * @param defaultValue 默认值
+     * @return 转换为defaultValue的t
+     * @param <T>
+     */
+    public static <T> T getOrDefault(Object t,@NonNull T defaultValue){
         return Convert.convert(defaultValue.getClass(), t, defaultValue);
     }
     public static <T> T getOrDefaultByRequest(HttpServletRequest request,String parameterName,T defaultValue){
-        return getOrDefault(request.getParameter(parameterName),defaultValue);
+        return getOrDefault(
+                Optional.ofNullable(request.getAttribute(parameterName))
+                        .orElseGet(()->request.getParameter(parameterName))
+                ,defaultValue);
     }
 }
